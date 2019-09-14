4404b02df2e7f8f753da12c6e307adae1c76cc3f
Naimur Hasan <37500414+naimurhasan@users.noreply.github.com>
2019-09-15 04:19:27 +0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='.')
+
+@client.event
+async def on_ready():
+    print('We have successfully logged in as {0.user.name}'.format(client))
+
+@client.command()
+async def ping(ctx):
+    """Say pong"""
+    await ctx.send('Pong [{}ms]'.format(round(client.latency*1000, 1)))
+
+@client.command()
+async def introduce(ctx):
+    """I'll introduce myself"""
+    await ctx.send('Hey, I\'m a discrod bot! Made by Naimur&Arefin.')
+
+client.run('NjIyMDk2MjYwNzY5MjUxMzI4.XXu6kw.8SjX6MOhkcVmb-zj9PytNvQcO2g')
+
